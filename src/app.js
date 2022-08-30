3700c32c331908f170b14fc62f45035e55d9de9e
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-08-30 23:00:05 +0000

    Progress in app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function(
+  document.qerySelector (#excuse). innerHTML =ExcuseGen ();
+  console.log "Test+ExcuseGen";
+) {};
+
+
+ let ExcuseGen =() => {
+  let pronoun = ["The" "A"]
+  let subject = ["dog" "cat" "my pet" ]
+  let verb = ["ate" "stole" "run away with my "]
+  let item = ["homework" "assigment" ]
+  let place = ["at my home" "in my yard"]
+}
+
